ecdacd088ad76a3fd9f8d93f070e8f9fc7d47597
Crowdbotics <team@crowdbotics.com>
2023-11-12 03:19:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "prioritizer-44495",
+  url: "https://prioritizer-44495.botics.co",
+  api: "https://prioritizer-44495.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
